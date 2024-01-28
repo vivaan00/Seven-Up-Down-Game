ee959e1ec323a2018996a3c888b12fbeff006ec0
vivaan00 <singhivivaan@gmail.com>
2024-01-28 17:12:15 +0530

    ok

--- App.js
+++ App.js
@@ -1,6 +1,27 @@
+let userChoice
+let msg
+
 function upClicked(){
-    alert('Up clicked')
+    userChoice = 1
+    generate()
 }
+
 function downClicked(){
-    alert('Down clicked')
+    userChoice = 0
+    generate()
+}
+
+function generate(){
+    let d1 = Math.floor(Math.random() * 6) + 1;
+    let d2 = Math.floor(Math.random() * 6) + 1;
+    if (d1+d2 < 7 && userChoice==0)
+        msg='You Won'
+
+    else if (d1+d2 > 7 && userChoice==1)
+        msg='You Won'
+        
+    else
+        msg='Better Luck Next Time'
+    document.getElementById('d').innerHTML = d1 + '&emsp;' + d2
+    document.getElementById('msg').innerHTML = msg
 }
